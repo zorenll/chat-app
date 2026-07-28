e5a06daf3267ec119a9ca2a33a508ab1ae31c42b
zoren leitso <zoren@zorens-MacBook-Pro.local>
2026-07-29 00:46:02 +0200

    Implemented TCP message protocol

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::Write,io::Read,net::TcpStream};
+use std::{io::Read, io::Write, net::TcpListener, net::TcpStream};
 
 // Message protocol
 // Field             Size
@@ -32,7 +32,7 @@ impl Message {
         return out;
     }
 
-    fn decode(data: Vec<u8>) -> Message {
+    fn decode(data: Vec<u8>) -> Result<Message, &'static str> {
         let mut address_length = [0; 2];
         address_length.copy_from_slice(&data[0..2]);
         let address_length: u16 = u16::from_be_bytes(address_length);
@@ -55,17 +55,37 @@ impl Message {
 
         let body = String::from_utf8(body).unwrap();
 
-        let out = Message { address, body };
+        let message = Message { address, body };
 
-        out
+        Ok(message)
     }
 }
-fn main() {
-    let hello = Message {
-        address: "anna".to_string(),
-        body: "hello anna".to_string(),
-    };
 
-    let listens
-    
+// Stream write protocol Packet1: Message size; Packet2: Message
+fn main() {
+    let ip = "127.0.0.1:4921";
+    let listener = TcpListener::bind(ip).expect("Failed to bind to port");
+
+    for stream in listener.incoming() {
+        let mut stream = stream.expect("Failed to stream man");
+
+        let mut message_length: [u8; 2] = [0; 2];
+        stream
+            .read(&mut message_length)
+            .expect("Failed to read from stream");
+        let message_length: u16 = u16::from_be_bytes(message_length);
+        let message_length: usize = usize::from(message_length);
+
+        let mut message: Vec<u8> = vec![0; message_length];
+        stream
+            .read_exact(&mut message)
+            .expect("Failed to read message");
+        if message.len() != message_length {
+            eprintln!("Did not read full message");
+        }
+
+        let message = Message::decode(message).expect("Failed to decode message");
+
+        println!("To: {}\nText: {}", message.address, message.body);
+    }
 }
